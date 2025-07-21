1da6e94d97c22125a91684e78d4a11c841a72217
Ayhem Hamdi <146385305+ayhem8hamdi@users.noreply.github.com>
2025-07-21 21:07:51 +0100

    Create responsive_image_widget.dart

---
+++ responsive_image_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveImage extends StatelessWidget {
+  final ImageProvider imageProvider; // AssetImage, NetworkImage, etc.
+  final double imageDesignWidth; // Original design width (required)
+  final double screenDesignWidth; // Design screen width (e.g., 375)
+  final double imageDesignAspectRatio; // width/height (required)
+  final BoxFit fit;
+  final double? maxWidth;
+  final double? minWidth;
+
+  const ResponsiveImage({
+    super.key,
+    required this.imageProvider,
+    required this.imageDesignWidth,
+    required this.screenDesignWidth,
+    required this.imageDesignAspectRatio,
+    this.fit = BoxFit.cover,
+    this.maxWidth,
+    this.minWidth,
+  });
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final responsiveWidth =
+        (imageDesignWidth / screenDesignWidth) * screenWidth;
+
+    final clampedWidth = responsiveWidth.clamp(
+      minWidth ?? imageDesignWidth * 0.5,
+      maxWidth ?? imageDesignWidth * 1.5,
+    );
+
+    return SizedBox(
+      width: clampedWidth,
+      child: AspectRatio(
+        aspectRatio: imageDesignAspectRatio,
+        child: Image(
+          image: imageProvider,
+          fit: fit,
+        ),
+      ),
+    );
+  }
+}
